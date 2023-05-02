bbe4c11d07a5a8a6dd93efd2e4bc12cb9e8c1af4
Karl Anderson <kra@monkey.org>
2023-05-02 10:33:52 -0700

    test tweak

--- twilio/packages/dialplans/outgoing/top.py
+++ twilio/packages/dialplans/outgoing/top.py
@@ -1,12 +1,6 @@
 from twilio.twiml.voice_response import VoiceResponse
 
 
-xml = """<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-    <Say>Hello, world!</Say>
-</Response>
-"""
-
 def top(event):
     response = VoiceResponse()
     response.say('Hello World')
