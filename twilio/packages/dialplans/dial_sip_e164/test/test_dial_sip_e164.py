5b4f57618073dc43abe3751d5643749e7fb5ee87
Karl Anderson <kra@monkey.org>
2023-05-10 21:30:19 -0700

    implement test

--- twilio/packages/dialplans/dial_sip_e164/test/test_dial_sip_e164.py
+++ twilio/packages/dialplans/dial_sip_e164/test/test_dial_sip_e164.py
@@ -2,13 +2,10 @@ from unittest import mock, TestCase
 import dial_sip_e164
 
 
-out = {'headers':
-       {'Content-Type': 'text/xml'},
-       'statusCode': 200,
-       'body': '<?xml version="1.0" encoding="UTF-8"?><Response><Dial '
-       'action="api_host/api/v1/web/namespace/dialplans/metric_dialer_status" '
-       'answerOnBridge="true">'
-       '<Sip>sip:operator@futel-stage.phu73l.net;region=us2?x-callerid=+19713512383&amp;x-enableemergency=false</Sip></Dial></Response>'}
+out={'headers':
+     {'Content-Type': 'text/xml'},
+     'statusCode': 200,
+     'body': '<?xml version="1.0" encoding="UTF-8"?><Response><Dial action="api_host/api/v1/web/namespace/dialplans/metric_dialer_status" answerOnBridge="true" callerId="5035551212"><Sip>sip:test@direct-futel-nonemergency-stage.sip.us1.twilio.com;</Sip></Dial></Response>'}
 
 
 class TestDialSip(TestCase):
