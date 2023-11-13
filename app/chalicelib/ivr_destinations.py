b47ecf9d108bdb506faf671b5cc553ac47c8b25c
Karl Anderson <oscule@protonmail.com>
2023-11-12 16:28:43 -0800

    reorder code for bugfix

--- app/chalicelib/ivr_destinations.py
+++ app/chalicelib/ivr_destinations.py
@@ -6,8 +6,6 @@ from twilio.twiml.voice_response import VoiceResponse
 
 #from . import util
 
-DESTINATIONS = {'enqueue_operator': enqueue_operator}
-
 def get_destination(name):
     """Return the destination function for name."""
     return DESTINATIONS.get(name)
@@ -23,3 +21,5 @@ def enqueue_operator(extension, request, env):
     # XXX statement should be an IVR statement for consistency
     #response.play('xxx') # please hold, must expand sound_url
     response.enqueue('operator') # wait_url, action
+
+DESTINATIONS = {'enqueue_operator': enqueue_operator}
