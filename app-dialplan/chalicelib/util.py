da77e36baf4524545f59787510ff2700e34bf27d
Karl Anderson <kra@monkey.org>
2024-09-13 19:29:10 -0700

    remove cruft

--- app-dialplan/chalicelib/util.py
+++ app-dialplan/chalicelib/util.py
@@ -195,32 +195,6 @@ def pstn_number(number, enable_emergency):
         return None
     return number
 
-def request_to_endpoint(request, env):
-    """
-    Return the endpoint responsible for the request.
-    """
-    extension = sip_to_user(request.post_fields['From'])
-    if extension:
-        # Call from client to Twilio SIP Domain,
-        # From is SIP URI of caller's extension.
-        return extension
-    extension = e164_to_extension(request.post_fields['To'], env['extensions'])
-    if extension:
-        # Incoming PSTN call to Twilio phone number,
-        # To is E.164 being called.
-        return extension
-    extension = e164_to_extension(
-        request.post_fields['From'], env['extensions'])
-    if extension:
-        # Outgoing PSTN call started by the Twilio client, where From is
-        # whatever we tell the client it is, so hopefully it is the E.164 of the
-        # "extension" "doing the thing".
-        return extension
-    # Is this an unknown state, do we want to survive this?
-    log("Can't find extension to:{} from:{}".format(
-        request.post_fields['To'], request.post_fields['From']))
-    return None
-
 def dial_pstn(to_number, from_extension, request, env):
     """Return TwiML to dial PSTN number with attributes from request."""
     caller_id = from_extension['caller_id']
