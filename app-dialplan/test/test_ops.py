e2edb17329bf1d7ab21aebbb2751d9b79aca07ea
Karl Anderson <kra@monkey.org>
2024-10-26 15:39:34 -0700

    env tweak

--- app-dialplan/test/test_ops.py
+++ app-dialplan/test/test_ops.py
@@ -2,7 +2,7 @@ from unittest import mock, TestCase
 
 from chalicelib import ops
 
-env = {'AWS_LOGS_TOPIC_ARN': 'AWS_LOGS_TOPIC_ARN',
+env = {#'AWS_LOGS_TOPIC_ARN': 'AWS_LOGS_TOPIC_ARN',
        'AWS_METRICS_TOPIC_ARN': 'AWS_METRICS_TOPIC_ARN',
        'TWILIO_ACCOUNT_SID': 'TWILIO_ACCOUNT_SID',
        'TWILIO_AUTH_TOKEN': 'TWILIO_AUTH_TOKEN',
