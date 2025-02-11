8616ab844dabcaea9ae8c11b620db540e512f0b6
Karl Anderson <kra@monkey.org>
2025-02-11 12:06:28 -0800

    move arg up for testing

--- app-ops/itest/test_exercise.py
+++ app-ops/itest/test_exercise.py
@@ -5,11 +5,12 @@ from chalicelib import ops
 
 env = env_util.get_env()
 
-stage = "prod"
+stage = "stage"
 extension = "test-one"
+
 
 class TestOps(TestCase):
 
     def test_exercise(self):
         """Exercise an arbitrary extension on stage."""
-        call = ops.exercise_one("stage", "clinton", env)
+        call = ops.exercise_one(stage, extension, "community_outgoing", env)
