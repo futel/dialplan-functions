7f9d7d244280717d8732e7078b275fa845bfacde
Karl Anderson <kra@monkey.org>
2025-12-19 12:25:03 -0800

    increase ops call rate

--- app-ops/app.py
+++ app-ops/app.py
@@ -17,8 +17,6 @@ def post_fields(request):
 # We want a rate that is likely to hit every extension at least once a day,
 # is unlikely to hit one extension several times in a row, and runs often.
 # We assueme that there isn't a problem with resources being used by the calls.
-# Every half an hour will handle up to 24 extensions if they are selected
-# evenly.
-@app.schedule(Rate(30, unit=Rate.MINUTES))
+@app.schedule(Rate(10, unit=Rate.MINUTES))
 def exercise(event):
     return ops.exercise(event, env)
